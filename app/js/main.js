c8653a13a2a4e388423c0691254cc00d4fbf3e69
206624202 <a206624202@pstadsc92pmlvml.tfayd.com>
2021-01-07 15:36:35 -0500

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -12,25 +12,12 @@ Highcharts.setOptions({
     }
 });
 
-let chartId = document.getElementById("chart-container");
+let chartIdTopStocks = document.getElementById("chart-container-top-stocks-2020");
 
 // checks for the chart ID and displays a backup image if the browser can't find it
-setTimeout(function() {
-    if(chartId.innerHTML === "") {
-        // console.log('noId');
-        let chartArea = document.getElementsByClassName("chart-area");
-        for(var i = 0; i < chartArea.length; i++) {
-            chartArea[i].style.display = "none";
-        } 
-        // insert chart screenshot here
-        document.getElementById("chart-fallback").innerHTML += '<img src="https://fm-static.cnbc.com/awsmedia/chart/2019/10/08/chart-error_wide.1570569331252.png" style="width: 100%;max-width:660px">';
-    } else {
-        // console.log('yesId')
-    }
-},500);
 
 function drawHighcharts() {
-    Highcharts.chart(chartId, {
+    Highcharts.chart(chartIdTopStocks, {
         chart: {
             type: 'bar',
             styledMode: true,
@@ -43,7 +30,7 @@ function drawHighcharts() {
             text: null
         },
         data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
+            googleSpreadsheetKey: '16o2V9k_Yxb4F21u6A5YXBVlGualKV1y2jMDLpJxmhZU'
         },
         // for bar charts only
         plotOptions: {
@@ -69,11 +56,7 @@ function drawHighcharts() {
         //     }
         // },
         legend: {
-            align: 'right',
-            symbolRadius: 0,
-            verticalAlign: 'top',
-            x: 10,
-            itemMarginTop: -10
+            enabled: false
         },
         xAxis: {
             labels: {
@@ -104,7 +87,8 @@ function drawHighcharts() {
         },
         tooltip: {
             shadow: false,
-            padding: 10
+            padding: 10,
+            valueSuffix: '%'
         },
         responsive: {
             rules: [{
@@ -113,11 +97,116 @@ function drawHighcharts() {
             },
             chartOptions: {
                 chart: {
-                spacingRight: 10
+                spacingRight: 15
                 },
                 legend: {
                     align: 'left',
                     x: -18
+                },
+                tooltip: {
+                    enabled: false
+                }
+            }
+            }]
+        }
+    }),
+    Highcharts.chart('chart-container-top-stocks-2020-2', {
+        chart: {
+            type: 'line',
+            styledMode: true,
+            spacingBottom: 25,
+            spacingRight: 100,
+            spacingLeft: 0,
+            spacingTop: 20
+        }, 
+        title: {
+            text: null
+        },
+        data: {
+            googleSpreadsheetKey: '16o2V9k_Yxb4F21u6A5YXBVlGualKV1y2jMDLpJxmhZU',
+            googleSpreadsheetWorksheet: 2
+        },
+        // for bar charts only
+        // plotOptions: {
+        //     series: {
+        //         groupPadding: 0.1
+        //     } 
+        // },
+        // for line charts only
+        plotOptions: {
+            series: {
+                lineWidth: 1,
+                // clip: false,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    fillColor: '#ffffff',
+                    states: {
+                        hover: {
+                            fillColor: '#ffffff'
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            symbolRadius: 0,
+            verticalAlign: 'top',
+            x: 10,
+            y: -20,
+            floating: true,
+            layout: 'vertical',
+            itemMarginBottom: 60,
+            itemMarginTop: 40
+        },
+        xAxis: {
+            labels: {
+                style: {
+                    whiteSpace: 'nowrap'
+                }
+            },
+            tickLength: 5,
+            // edits xAxis ticks
+            // dateTimeLabelFormats: {
+            //     week: '%b. %e',
+            // },
+            // tickInterval: 24 * 3600 * 1000 * 7
+        },
+        yAxis: {
+            title: false,
+            labels: {
+                useHTML: true,
+                overflow: 'allow'
+            },
+            tickAmount: 6,
+            max: 75,
+            min: -50
+            // adds commas to thousands
+            // formatter: function () {
+            //     return Highcharts.numberFormat(this.value,0,'.',',');
+            // },
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            shadow: false,
+            padding: 10,
+            valueSuffix: '%',
+            valueDecimals: 1,
+            shared: true
+        },
+        responsive: {
+            rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                chart: {
+                spacingRight: 15
+                },
+                legend: {
                 },
                 tooltip: {
                     enabled: false
